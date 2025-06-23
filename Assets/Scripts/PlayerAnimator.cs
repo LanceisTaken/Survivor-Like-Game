11e6c54086c1d182fa8982eeb0850076073feed3
Goh Yin Xu <gamermusic63@gmail.com>
2025-06-23 21:32:28 +0800

    IdleAnimation Added

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -7,6 +7,9 @@ public class PlayerAnimator : MonoBehaviour
     PlayerMovement pm;
     SpriteRenderer sr;
 
+    private float idleTimer = 0f;
+    private bool isIdle = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,16 +25,39 @@ public class PlayerAnimator : MonoBehaviour
         {
             am.SetBool("Move", true);
             spriteDirectionChecker();
+
+            // IMMEDIATELY reset idle state when movement starts
+            if (isIdle)
+            {
+                am.SetBool("Idling", false);
+                isIdle = false;
+            }
+
+            // Reset idle timer when moving
+            idleTimer = 0f;
         }
         else
         {
             am.SetBool("Move", false);
+
+            // Only count idle time if we're not already idling
+            if (!isIdle)
+            {
+                idleTimer += Time.deltaTime;
+
+                // Check if 5 seconds have passed
+                if (idleTimer >= 5f)
+                {
+                    isIdle = true;
+                    am.SetBool("Idling", true);
+                }
+            }
         }
 
         // Trigger Inspect animation on 'Y' key press
         if (Input.GetKeyDown(KeyCode.Y))
         {
-            am.Play("PlayerInspect", 0, 0f); // Reset and play the animation from the beginning
+            am.Play("PlayerInspect", 0, 0f);
         }
     }
 

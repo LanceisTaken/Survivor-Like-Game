52c01d1dfbd23c78657f70ed58c1281f6916fa5f
Goh Yin Xu <gamermusic63@gmail.com>
2025-08-05 17:07:52 +0800

    Fixed Prop Layers

--- Assets/Scripts/PropRandomizer.cs
+++ Assets/Scripts/PropRandomizer.cs
@@ -25,6 +25,24 @@ public class NewMonoBehaviourScript : MonoBehaviour
             int rand = Random.Range(0, propPrefabs.Count);
             GameObject prop = Instantiate(propPrefabs[rand], sp.transform.position, Quaternion.identity);
             prop.transform.parent = sp.transform;
+            prop.layer = LayerMask.NameToLayer("Terrain");
+
+            // Get ALL Sprite Renderers in the prop (including children)
+            SpriteRenderer[] allSpriteRenderers = prop.GetComponentsInChildren<SpriteRenderer>();
+
+            if (allSpriteRenderers.Length > 0)
+            {
+                foreach (SpriteRenderer sr in allSpriteRenderers)
+                {
+                    sr.sortingLayerName = "Props";
+                    sr.sortingOrder = 0; // You can also set a specific order
+                    Debug.Log($"Set sorting layer for {sr.gameObject.name} to Props");
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"Spawned prop '{prop.name}' does not have any Sprite Renderers!");
+            }
         }
     }
 }
